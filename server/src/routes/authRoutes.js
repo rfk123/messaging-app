27a70189216816b45d2b6266215f6128c8550a5d
rfk123 <rfkamali@gmail.com>
2026-05-13 19:30:33 -0400

    Implemented a login route that validates req body data, checks if pwd matches, and creates a token for user. responds with user data, message, and token.

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,14 +1,17 @@
 /*
 Define the URL and HTTP method.
 Here we are basically saying that when someone posts to /signup we run the signup controller and the controller holds the signup logic
+This file defines the available endpoints and which controller function should handle each one.
+essentially a traffic controller 
 */
 
 const express = require("express");
-const { signup } = require("../controllers/authControllers");
+const { signup, login } = require("../controllers/authControllers");
 
 const router = express.Router(); //create a mini-router just for auth routes
 
 router.post("/signup", signup); // when someone sends a post request to /signup, run the signup function
+router.post("/login", login);
 
 module.exports = router;
 
