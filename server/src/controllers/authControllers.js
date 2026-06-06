7f2ee18738b5c6ebccd736cdcf934e2c72862eb9
rfk123 <rfkamali@gmail.com>
2026-06-06 14:17:45 -0700

    Created the controller and route for a new search user route. Also added test cases for our login route

--- server/src/controllers/authControllers.js
+++ server/src/controllers/authControllers.js
@@ -137,7 +137,7 @@ const getMe = async (req, res) => {
             });
         }
 
-        return res.json({
+        return res.status(200).json({
             user: result.rows[0],
         });
     } catch (error) {
